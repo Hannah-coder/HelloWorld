8aba44f1a0eff84bfba4c91078fcf161986d7246
David <nelsondk@etsu.edu>
2021-10-04 21:39:08 -0400

    Created PageView.cs and ServerInformation.cs models

---
+++ Models/PageView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace API.Models
+{
+    public record PageView
+    {
+        public int Id { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public string URLSection1 { get; set; }
+        public string URLSection2 { get; set; }
+        public string Parameter1 { get; set; }
+        public string Parameter2 { get; set; }
+    }
+}
---
+++ Models/ServerInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace API.Models
+{
+    public record ServerInformation
+    {
+        public int Id { get; set; }
+        public TimeSpan UpTime { get; set; }
+        public string RuntimeVersion { get; set; }
+        public string OSVersion { get; set; }
+        public DateTime DateAndTime { get; set; }
+    }
+}
