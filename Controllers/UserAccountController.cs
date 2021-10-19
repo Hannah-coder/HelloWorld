a82a63a6b8bdcbc00d9869d80027022d6be84359
MikeShaw117 <85390154+MikeShaw117@users.noreply.github.com>
2021-10-19 17:35:02 -0400

    Added User

---
+++ Controllers/UserAccountController.cs
@@ -0,0 +1,45 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAccountController : Controller
+    {
+        private readonly ILogger<UserAccountController> _logger;
+        private readonly MetricsDbContext _context;
+
+        public UserAccountController(ILogger<UserAccountController> logger,
+            MetricsDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<List<UserAccounts>> GetUserAccountsAsync()
+        {
+            return await _context.UserAccounts.ToListAsync();
+        }
+
+        [HttpPost]
+        public ActionResult<UserAccounts> CreateUserAccountAsync(UserAccounts user)
+        {
+            _context.UserAccounts.Add(user);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            _context.SaveChangesAsync();
+            return user;
+        }
+    }
+}
--- Migrations/20211019203505_Whatever.Designer.cs
+++ Migrations/20211019203505_Whatever.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Migrations
 {
     [DbContext(typeof(MetricsDbContext))]
-    partial class MetricsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211019203505_Whatever")]
+    partial class Whatever
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
